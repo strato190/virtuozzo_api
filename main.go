13e81957ce758f6051868f663871533188089e82
“strato190” <yura.kharuk@hotmail.com>
2017-12-05 14:53:20 +0200

    main cleaner

--- main.go
+++ main.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"io/ioutil"
-	"log"
 	"net/http"
 	"os"
 
@@ -12,8 +9,6 @@ import (
 )
 
 func main() {
-	var intf Instances
-	//var err error
 
 	r := mux.NewRouter()
 
@@ -24,38 +19,12 @@ func main() {
 	r.Handle("/vms", jwtMiddleware.Handler(VMHandler)).Methods("GET")
 	r.Handle("/get-token", GetTokenHandler).Methods("GET")
 	r.Handle("/vms", jwtMiddleware.Handler(VMAddHandler)).Methods("POST")
+	//r.Handle("/cts", jwtMiddleware.Handler(CTHandler)).Methods("GET")
+	//r.Handle("/cts", jwtMiddleware.Handler(CTAddHandler)).Methods("POST")
 
 	http.ListenAndServe(":3000", handlers.LoggingHandler(os.Stdout, r))
 
-	jsn, _ := ioutil.ReadFile("config.json")
-	json.Unmarshal(jsn, &intf)
-	if len(intf.Cts) > 0 {
-		for _, c := range intf.Cts {
-			if err := createCT(c); err != nil {
-				log.Fatal(err)
-			}
-			//err = configVMNetwork(v)
-			if err := configCT(c); err != nil {
-				log.Fatal(err)
-			}
-			//err = createCT(c)
-			//err = configCT(c)
-
-		}
-	}
-	if len(intf.Vms) > 0 {
-		for _, v := range intf.Vms {
-
-			if err := createVM(v); err != nil {
-				log.Fatal(err)
-			}
-			if err := configVM(v); err != nil {
-				log.Fatal(err)
-			}
-			if err := configVMNetwork(v); err != nil {
-				log.Fatal(err)
-			}
-		}
-	}
+	//jsn, _ := ioutil.ReadFile("config.json")
+	//json.Unmarshal(jsn, &intf)
 
 }
